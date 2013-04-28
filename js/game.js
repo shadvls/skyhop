0e4e09e3d00b0d74b63a06fe7ba60b18b68862a6
shadvls <yanshaaa28@gmail.com>
2013-04-29 00:00:00 +0700

    feat: add lerp utility function

--- js/game.js
+++ js/game.js
@@ -2,3 +2,4 @@ var c=document.getElementById("c"),ctx=c.getContext("2d");
 var W=320,H=480;
 function rand(a,b){return a+Math.random()*(b-a)}
 function clamp(v,mn,mx){return v<mn?mn:v>mx?mx:v}
+function lerp(a,b,t){return a+(b-a)*t}
