6184800848c013ffbb57ed8a122144bef10278ca
shadvls <yanshaaa28@gmail.com>
2013-04-30 00:00:00 +0700

    feat: define game configuration object

--- js/game.js
+++ js/game.js
@@ -3,3 +3,4 @@ var W=320,H=480;
 function rand(a,b){return a+Math.random()*(b-a)}
 function clamp(v,mn,mx){return v<mn?mn:v>mx?mx:v}
 function lerp(a,b,t){return a+(b-a)*t}
+var CFG={W:320,H:480,gravity:0.25,flap:-5.5,pipeW:40,pipeGap:120,pipeSpeed:2,groundH:80,birdR:12,birdX:60};
