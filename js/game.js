6520d5117789f4db4955c5d104736141ca373fb8
shadvls <yanshaaa28@gmail.com>
2013-05-01 00:00:00 +0700

    feat: define game state enum

--- js/game.js
+++ js/game.js
@@ -4,3 +4,4 @@ function rand(a,b){return a+Math.random()*(b-a)}
 function clamp(v,mn,mx){return v<mn?mn:v>mx?mx:v}
 function lerp(a,b,t){return a+(b-a)*t}
 var CFG={W:320,H:480,gravity:0.25,flap:-5.5,pipeW:40,pipeGap:120,pipeSpeed:2,groundH:80,birdR:12,birdX:60};
+var S={MENU:0,PLAYING:1,GAMEOVER:2};
