b5ea2ad76692eacaef70390e69347bd51fa4a97d
shadvls <yanshaaa28@gmail.com>
2013-06-23 00:00:00 +0700

    feat: scroll ground for motion illusion

--- js/game.js
+++ js/game.js
@@ -57,3 +57,4 @@ function drawSky(){if(!skyGrad)cacheSkyGradient();ctx.fillStyle=skyGrad;ctx.fill
 function drawStars(){for(var i=0;i<30;i++){var sx=(i*37+13)%W,sy=(i*53+7)%(groundY*0.6);var bright=0.3+0.7*Math.sin(starPhase+i*2);ctx.globalAlpha=bright;ctx.fillStyle="rgba(255,255,255,0.5)";ctx.fillRect(sx,sy,2,2)}ctx.globalAlpha=1}
 function updateStars(){starPhase+=0.02}
 function drawClouds(){ctx.fillStyle="rgba(255,255,255,0.03)";for(var i=0;i<3;i++){var cx=(i*120+cloudOffset)%(W+100)-50;ctx.beginPath();ctx.ellipse(cx,50+i*40,40,10,0,0,Math.PI*2);ctx.fill()}}
+function updateClouds(){cloudOffset-=0.2}
