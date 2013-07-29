d30082ec6006418eaa24890b6b27f7007b89a432
shadvls <yanshaaa28@gmail.com>
2013-07-30 00:00:00 +0700

    feat: check if skin is unlocked

--- js/game.js
+++ js/game.js
@@ -94,3 +94,4 @@ function updateSlowMo(){if(slowMo<1)slowMo=Math.min(slowMo+0.02,1)}
 function getPipeSpeed(){return Math.min(pipeSpeed+Math.floor(score/10)*0.2,6)}
 function getDynamicPipeSpeed(){return Math.min(pipeSpeed+Math.floor(score/8)*0.3,7)}
 function getDynamicPipeGap(){return Math.max(pipeGap-Math.floor(score/5)*2,80)}
+function getDynamicGravity(){return Math.min(gravity+Math.floor(score/15)*0.02,0.4)}
