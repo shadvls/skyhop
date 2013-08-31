d653eeb15bd2b8a133e1a8e0c14ab8eef0f1904c
shadvls <yanshaaa28@gmail.com>
2013-09-01 00:00:00 +0700

    feat: bind touch start event

--- js/game.js
+++ js/game.js
@@ -127,3 +127,4 @@ function checkOrientation(){landscapeWarn=window.innerWidth>window.innerHeight&&
 function drawOrientationWarn(){if(landscapeWarn){ctx.fillStyle="rgba(0,0,0,0.9)";ctx.fillRect(0,0,W,H);ctx.fillStyle="#fff";ctx.font="16px monospace";ctx.textAlign="center";ctx.fillText("Please rotate your device",W/2,H/2)}}
 function onTap(e){var pos=getPos(e);initAudio();if(restartDelay>0)return;if(state==S.MENU){handleSkinClick(pos.x,pos.y);handleDiffClick(pos.x,pos.y)}else if(state==S.PLAYING){if(!stats.games)unlockAchievement("firstFlap");birdFlap();playFlap();emitBurst(birdX,birdY,3,"rgba(255,255,255,0.5)")}else if(state==S.GAMEOVER&&restartDelay<=0){resetGame()}}
 function onTouchStart(e){e.preventDefault();if(e.touches.length>1)return;touchTimer=setTimeout(function(){touchTimer=null},500)}
+function onTouchEnd(e){if(touchTimer&&e.changedTouches.length==1){clearTimeout(touchTimer);touchTimer=null;onTap(e)}}
