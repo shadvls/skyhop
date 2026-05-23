00519c2655d46cb4a2ba51348d424e80cb8e4bfc
sha-wrks <yanshaaa28@gmail.com>
2026-05-24 01:26:41 +0700

    Add GameStatus and AsteroidSize enums

---
+++ VoidStrike/Models/Enums.cs
@@ -0,0 +1,17 @@
+namespace VoidStrike.Models;
+
+public enum GameStatus
+{
+    Menu,
+    Playing,
+    Paused,
+    GameOver,
+    LevelComplete
+}
+
+public enum AsteroidSize
+{
+    Large = 40,
+    Medium = 20,
+    Small = 10
+}
